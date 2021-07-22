16c1616749286a992c2a4f2ebd069d9c3ede51be
elvisjianglol <54383240+elvisjianglol@users.noreply.github.com>
2021-07-22 10:25:49 -0700

    Update script

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hope git works
+        Debug.Log("hello there");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
