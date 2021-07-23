0b919aa9b67aa8bdd392000ecdc5bf97d974b55d
elvisjianglol <54383240+elvisjianglol@users.noreply.github.com>
2021-07-22 22:23:11 -0700

    Add PlayerMovement to Player

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    
+
+    Vector3 moveDirection;
+
+    float horizontalMovement;
+    float verticalMovement;
+    public float movementMultiplier = 6f;
+    public float rbDrag = 5f;
+    public float moveSpeed = 10f;
+
+    Rigidbody rb;
+
+    // get rb object
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MyInput();
+        ControlDrag();
+    }
+
+    private void MyInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+    }
+
+    private void FixedUpdate()
+    {
+        // No need for deltaTime
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // normalize
+        rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+    }
+
+    void ControlDrag()
+    {
+        rb.drag = rbDrag;
     }
 }
