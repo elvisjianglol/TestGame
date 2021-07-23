e570da962f35486f57078340f1d55146cc972e60
elvisjianglol <54383240+elvisjianglol@users.noreply.github.com>
2021-07-22 20:30:19 -0700

    Add playermovement script/ remove old script

--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello hope this works");
+        
     }
 
     // Update is called once per frame
