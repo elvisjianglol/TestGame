01ca36cfb1ba6786575d8d11c208481fa6138d91
elvisjianglol <54383240+elvisjianglol@users.noreply.github.com>
2021-07-25 12:50:01 -0700

    Added Jump

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -5,32 +5,66 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    
-
     Vector3 moveDirection;
-
     float horizontalMovement;
     float verticalMovement;
     public float movementMultiplier = 6f;
     public float rbDrag = 5f;
-    public float moveSpeed = 10f;
-
+    public float offGroundDrag = 1f;
+    public float storedMoveSpeed = 10f;
+    public float airSpeed = 2f;
     Rigidbody rb;
 
-    // get rb object
+    //vars that change
+    public float moveSpeed;
+
+    //jumping variables
+    public float jumpMultiplier = 5f;
+    //groundCheck vars
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+    public Transform groundCheckObj;
+    bool onGround;
+
+
 
     private void Start()
     {
+        moveSpeed = storedMoveSpeed;
+        // get rb object
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
     }
+
+
+
 
     private void Update()
     {
+
+        onGround = Physics.CheckSphere(groundCheckObj.position, groundDistance, groundMask);
         MyInput();
         ControlDrag();
+
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            PlayerJump();
+        }
+        if(!onGround)
+        {
+            rb.drag = offGroundDrag;
+            moveSpeed = airSpeed;
+            
+        }
+
+        else
+        {
+            moveSpeed = storedMoveSpeed;
+        }
     }
+
+
+
 
     private void MyInput()
     {
@@ -40,11 +74,17 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
     }
 
+
+
+
     private void FixedUpdate()
     {
         // No need for deltaTime
         MovePlayer();
     }
+
+
+
 
     void MovePlayer()
     {
@@ -52,8 +92,23 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
     }
 
+
+
+
     void ControlDrag()
     {
         rb.drag = rbDrag;
     }
+
+
+
+
+    void PlayerJump()
+    {
+        rb.AddForce(transform.up * jumpMultiplier, ForceMode.VelocityChange);
+    }
+
+
+
+
 }
